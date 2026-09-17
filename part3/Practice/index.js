22a2a551bde0271bdc58f93a5a71a3a1dca004c8
tapishu <75188634+tapishu@users.noreply.github.com>
2026-09-17 13:57:48 -0700

    upd

--- part3/Practice/index.js
+++ part3/Practice/index.js
@@ -28,9 +28,9 @@ let notes = [
   },
 ];
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
+// app.get("/", (request, response) => {
+//   response.send("<h1>Hello World!</h1>");
+// });
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
