8189e1458c11065fb73139c8e444cc16b6773bca
tapishu <75188634+tapishu@users.noreply.github.com>
2026-09-18 10:58:08 -0700

    add

--- part3/PhoneBackend/index.js
+++ part3/PhoneBackend/index.js
@@ -45,9 +45,9 @@ let persons = [
 ]
 
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
+// app.get("/", (request, response) => {
+//   response.send("<h1>Hello World!</h1>");
+// });
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
