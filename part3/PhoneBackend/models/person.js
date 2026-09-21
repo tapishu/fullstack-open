52faf322182df4033ecadef435b6dd48e315f914
tapishu <75188634+tapishu@users.noreply.github.com>
2026-09-21 14:25:34 -0700

    Add files via upload

--- part3/PhoneBackend/models/person.js
+++ part3/PhoneBackend/models/person.js
@@ -31,7 +31,7 @@ const personSchema = new mongoose.Schema({
     required : true,
     validate:{
         validator: function(v){
-            return /ˆ\d{2,3}-\d+$/.test(v)
+            return /^\d{2,3}-\d+$/.test(v)
         },
         message: props => `${props.value} is invalid '09-1234567' or '040-1234567'`
     }
