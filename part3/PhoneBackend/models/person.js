4a398bff86f2aac414623f49abb0ed7727f5dff5
tapishu <75188634+tapishu@users.noreply.github.com>
2026-09-20 21:08:17 -0700

    Add files via upload

--- part3/PhoneBackend/models/person.js
+++ part3/PhoneBackend/models/person.js
@@ -18,18 +18,18 @@ mongoose.connect(url, { family: 4 })
   })
 
 const personSchema = new mongoose.Schema({
-  id: String,
+//   id: String,
   name: String,
   number: String,
 })
 
-// noteSchema.set('toJSON', {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString()
-//     delete returnedObject._id
-//     delete returnedObject.__v
-//   }
-// })
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 
 module.exports = mongoose.model('Person', personSchema)
