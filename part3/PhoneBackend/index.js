d2cd7dc72e0a83ecae7fde327d9eecd250996fc1
tapishu <75188634+tapishu@users.noreply.github.com>
2026-09-21 10:38:03 -0700

    Add files via upload

--- part3/PhoneBackend/index.js
+++ part3/PhoneBackend/index.js
@@ -60,7 +60,7 @@ Person.find({}).then(persons => {
 })
 });
 
-app.get("/api/persons/:id",(request, response)=>{
+app.get("/api/persons/:id",(request, response, next)=>{
 //     const id = request.params.id
 //     const person = persons.find((p)=> p.id ===id)
 //     console.log("pass")
@@ -74,6 +74,7 @@ if (person){
     response.status(404).end()
 }
   })
+  .catch(error => next(error))
 })
 
 app.get("/info", (request, response) => {
@@ -128,13 +129,41 @@ app.post("/api/persons",(request,response) =>{
     // response.json(person)
 })
 
-app.delete("/api/persons/:id",(request,response) =>{
+app.delete("/api/persons/:id",(request,response,next) =>{
 // const id = request.params.id
 // persons = persons.filter((p)=>p.id !==id)
 Person.findByIdAndDelete(request.params.id).then(result =>{
 response.status(204).end()
 })
+.catch(error => next(error))
 })
+
+app.put("/api/persons/:id",(request,response,next)=>{
+    const {name,number} =request.body
+
+    Person.findById(request.params.id).then(person =>{
+        if(!person){
+            return response.status(404).end()
+        }
+        person.name =name
+        person.number = number
+        return person.save().then((updatedPerson)=>{
+            response.json(updatedPerson)
+        })
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
